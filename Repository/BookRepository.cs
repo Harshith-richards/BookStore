8cb725cc0801d6c2ef1d7d25c2ed933b60cbaf7d
Harshith-richards <harshithkv717@gmail.com>
2026-04-08 16:05:33 +0530

    Updateing the BookRepository

--- Repository/BookRepository.cs
+++ Repository/BookRepository.cs
@@ -1,6 +1,26 @@
-﻿namespace BookStore.Repository
+﻿namespace BookStore.Repository;
+using BookStore.Data;
+using BookStore.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+public class BookRepository : IBookRepository
 {
-    public class BookRepository : IBookRepository
-    {
-    }
+        private readonly BookStoreContext _context;
+        public BookRepository(BookStoreContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<BooksModel>> GetAllBooksAsync()
+        {
+           var records = await _context.Books.Select(x => new BooksModel()
+           {
+               Id = x.Id,
+               Title = x.Title,
+               Description = x.Description,
+           }).ToListAsync();
+
+           return records;
+        }
 }
+
