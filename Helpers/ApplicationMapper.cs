277d39be4c3e35cc4e05be2748cb2cfcb612a3c2
Harshith-richards <harshithkv717@gmail.com>
2026-04-09 15:34:50 +0530

    Adding the Helper folder for hte Mappers

--- Helpers/ApplicationMapper.cs
+++ Helpers/ApplicationMapper.cs
@@ -1,6 +1,18 @@
-﻿namespace BookStore.Helpers
+﻿using AutoMapper;
+using BookStore.Data;
+using BookStore.Models;
+
+namespace BookStore.Helpers
 {
-    public class ApplicationMapper
+    public class ApplicationMapper : Profile
     {
+        public ApplicationMapper()
+        {
+            // CreateMap<Source, Destination>();
+            // Example: CreateMap<BooksModel, BooksDTO>();
+            CreateMap<Books, BooksModel>().ReverseMap();
+
+
+        }
     }
 }
