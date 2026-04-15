dc68b3df1fbf0a1cbce2fa47d10eaef3ae37db0f
Harshith-richards <harshithkv717@gmail.com>
2026-04-15 12:20:08 +0530

    Adding the AccountController

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using BookStore.Models;
+using BookStore.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountRepository _accountRepository;
+        public AccountController(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+
+        }
+
+        [AllowAnonymous]
+        [HttpPost("signup")]
+        public async Task<IActionResult> SignUp([FromBody] SignUpModel model)
+        {
+            var result = await _accountRepository.SignUpAsync(model);
+            if (result.Succeeded)
+            {
+                return Ok("User created successfully");
+            }
+
+            return BadRequest(result.Errors); // 
+        }
+    }
+}
