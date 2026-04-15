a99af38a472625053a619e0340937cf240476649
Harshith-richards <harshithkv717@gmail.com>
2026-04-15 15:03:38 +0530

    Implementing the AccountRepository

--- Repository/AccountRepository.cs
+++ Repository/AccountRepository.cs
@@ -1,15 +1,23 @@
 ﻿using BookStore.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace BookStore.Repository
 {
     public class AccountRepository : IAccountRepository
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IConfiguration _configuration;
 
-        public AccountRepository(UserManager<ApplicationUser> userManager)
+        public AccountRepository(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IConfiguration configuration)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
         }
 
         public async Task<IdentityResult> SignUpAsync(SignUpModel model)
@@ -24,8 +32,41 @@ namespace BookStore.Repository
             };
 
             return await _userManager.CreateAsync(user, model.Password);
-
-
         }
+
+
+        public async Task<string> LoginAsync(SignInModel signInModel)
+        {
+            var result = await _signInManager.PasswordSignInAsync(signInModel.Email, signInModel.Password, false, false);
+            if (!result.Succeeded) 
+            {
+                return null; // Login successful, return null to indicate no error message
+            }
+
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, signInModel.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+             };
+            //var authSigningKey = new SymmetricSecurityKey(
+            //    Encoding.ASCII.GetBytes(_configuration["Jwt:Secret"])
+            //); // Replace with your actual secret key 
+
+            var authSigningKey = new SymmetricSecurityKey(
+                Encoding.ASCII.GetBytes(_configuration["Jwt:Secret"])
+            );
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:ValidIssuer"],
+                audience: _configuration["Jwt:ValidAudience"],
+                expires: DateTime.Now.AddHours(2),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+
     }
 }
