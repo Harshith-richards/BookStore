9e4d85c8bc194f91228e03345387c3b9ef4a89e9
Harshith-richards <harshithkv717@gmail.com>
2026-04-15 12:19:36 +0530

    Creating and implementing the SignUpAsync Repository

---
+++ Repository/AccountRepository.cs
@@ -0,0 +1,31 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookStore.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountRepository(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IdentityResult> SignUpAsync(SignUpModel model)
+        {
+            // to create a new user and save it to the database.
+            var user = new ApplicationUser
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Email = model.Email,
+                UserName = model.Email
+            };
+
+            return await _userManager.CreateAsync(user, model.Password);
+
+
+        }
+    }
+}
---
+++ Repository/IAccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookStore.Repository
+{
+    public interface IAccountRepository
+    {
+        Task<IdentityResult> SignUpAsync(SignUpModel model);
+    }
+}
